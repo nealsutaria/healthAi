aa4b9a3a912227e73bd960f763496394f8c52685
neal-sutaria <97972270+neal-sutaria@users.noreply.github.com>
2025-07-20 23:34:34 -0500

    Fixed reset password mail logic

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
-    user_name:            Rails.application.credentials.dig(:gmail, :username),
+    user_name:            Rails.application.credentials.dig(:gmail, :user_name),
     password:             Rails.application.credentials.dig(:gmail, :password),
     authentication:       "plain",
     enable_starttls_auto: true
